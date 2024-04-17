acdbddd131936319555defaa794c3977e1159429
Stephen Man <stephenman88@gmail.com>
2024-04-17 15:30:46 -0400

    initial commit

---
+++ src/app/lib/gundam-ab.js
@@ -0,0 +1,147 @@
+const gabSets = [
+    {
+        set_id: 0,
+        set_name: "season_01",
+        set_code: "AB01"
+    },
+    {
+        set_id: 1,
+        set_name: "season_02",
+        set_code: "AB02"
+    },
+    {
+        set_id: 2,
+        set_name: "season_03",
+        set_code: "AB03"
+    },
+    {
+        set_id: 3,
+        set_name: "season_04",
+        set_code: "AB04"
+    },
+    {
+        set_id: 4,
+        set_name: "linxtage_01",
+        set_code: "LX01"
+    },
+    {
+        set_id: 5,
+        set_name: "linxtage_02",
+        set_code: "LX02"
+    },
+    {
+        set_id: 6,
+        set_name: "linxtage_03",
+        set_code: "LX03"
+    },
+    {
+        set_id: 7,
+        set_name: "linxtage_04",
+        set_code: "LX04"
+    },
+    {
+        set_id: 8,
+        set_name: "unitribe_01",
+        set_code: "UT01"
+    },
+    {
+        set_id: 9,
+        set_name: "arsenal_rare_01_1"
+    },
+    {
+        set_id: 10,
+        set_name: "arsenal_rare_01_2"
+    },
+    {
+        set_id: 11,
+        set_name: "arsenal_rare_02_1"
+    },
+    {
+        set_id: 12,
+        set_name: "arsenal_rare_02_2"
+    },
+    {
+        set_id: 13,
+        set_name: "arsenal_rare_03_1"
+    },
+    {
+        set_id: 14,
+        set_name: "arsenal_rare_03_2"
+    },
+    {
+        set_id: 15,
+        set_name: "arsenal_rare_04_1"
+    },
+    {
+        set_id: 16,
+        set_name: "arsenal_rare_04_2"
+    },
+    {
+        set_id: 17,
+        set_name: "linxtage_rare_01"
+    },
+    {
+        set_id: 18,
+        set_name: "linxtage_rare_02"
+    },
+    {
+        set_id: 19,
+        set_name: "linxtage_rare_03"
+    },
+    {
+        set_id: 20,
+        set_name: "linxtage_rare_04"
+    },
+    {
+        set_id: 21,
+        set_name: "unitribe_booster_01"
+    },
+    {
+        set_id: 22,
+        set_name: "starter_deck_SEED"
+    },
+    {
+        set_id: 23,
+        set_name: "starter_deck_SEED_vol_2"
+    },
+    {
+        set_id: 24,
+        set_name: "starter_deck_iron-blooded"
+    },
+    {
+        set_id: 25,
+        set_name: "booster_pack_SEED_series"
+    }
+]
+
+const gabCards = [
+    {
+        id: 1,
+        set_id: "001",
+        rarity: "M",
+        code: "RX-78-2",
+        name: "Gundam",
+        type: "mobile suit",
+        behavior: "short range",
+        organization: "Earth Federation Space Force",
+        cost: 4,
+        mobility: 200,
+        long_range_atk: 110,
+        short_range_atk: 370,
+        hp: 380,
+        height: 18.0,
+        weight: 43.4,
+        pilot: "Amuro Ray",
+        main_weapon: "Beam Saber",
+        main_range: 1,
+        main_type: "short range",
+        sub_weapon: "Beam Rifle",
+        sub_range: 3,
+        sub_type: "long range",
+        ms_ability_name: "consecutive attacks",
+        ms_ability_activation: "on activate",
+        ms_ability_target: "single target (enemy)",
+        ms_ability_range: 2,
+        ms_ability_cost: 3
+    }
+]
